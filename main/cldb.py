8d09b381b92b6e6567950185ee979fcfe3473aa2
Jankie <notflyloong@outlook.com>
2023-12-29 13:22:52 +0800

    Add Invalid parameter tips

--- main/cldb.py
+++ main/cldb.py
@@ -14,6 +14,10 @@ def main(argc:int = len(sys.argv),argv:list = sys.argv) -> int:
     if argc == 1:
         print(welc)
         return 0
+    elif argc == 2:
+        print('Invalid parameter.')
+        return -1
+    
     return 0
 
 if __name__ == '__main__':
