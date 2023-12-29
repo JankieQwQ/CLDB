9bc4d9da38d30a36c7051a127aeb8970efe90b76
Jankie <notflyloong@outlook.com>
2023-12-29 13:31:24 +0800

    Add syntax check entry

--- main/cldb.py
+++ main/cldb.py
@@ -47,6 +47,16 @@ def main(argc:int = len(sys.argv),argv:list = sys.argv) -> int:
         print('Invalid parameter.')
         return -1
     
+    modelist = ['-s','-l']
+    filename = argv[1]
+    mode     = argv[2]
+    syntaxer = Syntax(filename)
+    if not mode in modelist:
+        print('Invalid parameter.')
+        return -1
+    if mode == '-s':
+        syntaxer.check()
+        print('Success!')
     return 0
 
 if __name__ == '__main__':
