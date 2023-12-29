f5d8c3567278d09e7b57fb1a055c8b48060b6b53
Jankie <notflyloong@outlook.com>
2023-12-29 13:21:19 +0800

    Add usage tips

---
+++ main/cldb.py
@@ -0,0 +1,21 @@
+import sys
+
+welc = '''
+CLDB - CELLO DEBUGGER
+
+Usage:
+    cldb [source file] [option]
+Options:
+    -s Syntax error check
+    -l Monitor vital signs (heartbeat, SpO2)
+'''
+
+def main(argc:int = len(sys.argv),argv:list = sys.argv) -> int:
+    if argc == 1:
+        print(welc)
+        return 0
+    return 0
+
+if __name__ == '__main__':
+    code = main()
+    raise SystemExit(code)
